6428368443ea492ccb5284a604e19422fe321646
shresthapr <shresthaprajan38@gmail.com>
2020-10-29 13:34:26 +0200

    2nd task: function components and class based component created, component connected to state

--- 2_task-margit/src/App.js
+++ 2_task-margit/src/App.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import Animal from './Component/Animal/Animal';
 import "./App.css";
 
-const App = () => {
-  return (
-    <div className="cards">
+class App extends Component {
+  state = {
+    animals: [{ name: "Elephant", img: "https://source.unsplash.com/0prglfrYY08" },
+    { name: "Lion", img: "https://source.unsplash.com/P0932ANXT0g" },
+    { name: "Zebra", img: "https://source.unsplash.com/Y0fha67MdO4" }
+    ]
+  };
+  render() {
+    return (
 
-      <Animal name="Elephant" img="" />
-      <Animal name="Lion" img="" />
-      <Animal name="Zebra" img="" />
-    </div>
-  );
-};
+      <div className="cards">
 
+        <Animal
+          name={this.state.animals[0].name}
+          img={this.state.animals[0].img}
+        />
+        <Animal
+          name={this.state.animals[1].name}
+          img={this.state.animals[1].img}
+        />
+        <Animal
+          name={this.state.animals[2].name}
+          img={this.state.animals[2].img}
+        />
+
+      </div>
+    );
+
+  }
+}
 export default App;
+
